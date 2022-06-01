d5b0dbdd3724e63dc44a9248f85c5f6a4c14d27a
Sevinj-Joy <seva.rasul15@gmail.com>
2022-06-01 16:28:16 +0400

    Updated

--- gui.py
+++ gui.py
@@ -1,4 +1,6 @@
+from fileinput import filename
 import tkinter as tk
+from tkinter import *
 from tkinter import Canvas, filedialog, Text
 import os
 
@@ -6,19 +8,23 @@ from click import open_file
 
 root=tk.Tk()
 
-canvas=tk.Canvas(root, height=700, width=700, bg="#263D42")
+def addApp():
+    filename=filedialog.askopenfilename(initialdir="/", title="Select File" , 
+    filetypes=(("executable", "*.exe"),("all files","*.*")))
+
+canvas=tk.Canvas(root, height=500, width=700, bg="#263D42")
 canvas.pack()
 
 frame =tk.Frame(root, bg="white")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
 openFile=tk.Button(root, text="Open File", padx=10,
-                 pady=5, fg="white", bg="#263D42")
-openFile.pack()
+                 pady=5, fg="white", bg="#263D42", command = addApp)
+openFile.pack(side="top")
 
 runApps=tk.Button(root, text="Run Apps", padx=10,
                  pady=5, fg="white", bg="#263D42")
-runApps.pack()
+runApps.pack(side="top")
 
 
 
