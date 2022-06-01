66462e8853d778da070f0f2ba0d4537d1dc00535
Sevinj-Joy <seva.rasul15@gmail.com>
2022-06-01 15:42:10 +0400

    Gui app started

---
+++ gui.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import Canvas, filedialog, Text
+import os
+
+root=tk.Tk()
+
+canvas=tk.Canvas(root, height=600, width=700, bg="#263D42")
+canvas.pack()
+root.mainloop()
